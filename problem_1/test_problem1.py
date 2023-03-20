412a405a57d5fe0a7f9e21a94b70d0f4eb24d60b
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-03-19 22:54:05 -0400

    all file changes

--- problem_1/test_problem1.py
+++ problem_1/test_problem1.py
@@ -1,8 +1,27 @@
 import unittest
+from problem1 import *
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestCase(unittest.TestCase):
+
+    # test to make sure that all the trains are included
+    # https://www.mbta.com/schedules/subway
+    def testFilteredTrains(self):
+        # gets the filtered_subway_routes data
+        list_of_filtered_subway_routes = filter_subway_routes()
+
+        # gets the long names of the subway_route
+        list_of_filtered_subway_routes_long_names = get_long_names(list_of_filtered_subway_routes)
+
+        self.assertTrue("Red Line" in list_of_filtered_subway_routes_long_names)
+        self.assertTrue("Mattapan Trolley" in list_of_filtered_subway_routes_long_names)
+        self.assertTrue("Orange Line" in list_of_filtered_subway_routes_long_names)
+        self.assertTrue("Blue Line" in list_of_filtered_subway_routes_long_names)
+        self.assertTrue("Green Line B" in list_of_filtered_subway_routes_long_names)
+        self.assertTrue("Green Line C" in list_of_filtered_subway_routes_long_names)
+        self.assertTrue("Green Line D" in list_of_filtered_subway_routes_long_names)
+        self.assertTrue("Green Line E" in list_of_filtered_subway_routes_long_names)
+
 
 if __name__ == '__main__':
     unittest.main()
